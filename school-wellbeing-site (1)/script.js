bd0302d9216716f62dd8c316b3d9d042307ed2e6
intuitiveclock4-hub <intuitiveclock4@gmail.com>
2025-11-09 06:17:16 +0300

    Add files via upload

---
+++ school-wellbeing-site (1)/script.js
@@ -0,0 +1,14 @@
+const App = () => (
+  <div style={{padding:'20px'}}>
+    <h1>School Wellbeing Website</h1>
+    <p>This is a static export version of the School Wellbeing React site.</p>
+    <ul>
+      <li>Home</li>
+      <li>Research</li>
+      <li>Results</li>
+      <li>Resources</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById('root'));
